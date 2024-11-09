b16b1d07bb84d72c3886c8ea6c8ae945303d14bd
Chinweike99 <innocentakwolu1@gmail.com>
2024-11-09 04:53:45 +0100

    items id

--- client/src/Components/FoodItem/FoodItem.jsx
+++ client/src/Components/FoodItem/FoodItem.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './FoodItem.module.css';
-import { foodList } from '../../assets/assts';
+import StarRateIcon from '@mui/icons-material/StarRate';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+
 
 const FoodItem = ({id, name, image,price, description, category}) => {
+  const [itemCount, setCount] = useState(0)
+
   return (
-    <div>
-        <img src={image} alt="" />
+    <div className={styles.foodItem}>
+        <div className={styles.imageContainer}>
+          <img src={image} alt="food-Image" />
+          {
+            !itemCount ? <button  onClick={()=>setCount(prev=>prev + 1)}>Add</button> 
+            : <div>
+              <RemoveIcon className={styles.remove} onClick={()=>setCount(prev=>prev-1)}/>
+              <p>{itemCount <= 1 ? `${itemCount} Portion` : `${itemCount} Portions`}</p>
+              <AddIcon className={styles.add} onClick={()=>setCount(prev=>prev+1)}/>
+            </div>
+          }
+        </div>
+
+        <div className={styles.foodInfo}>
+          <div className={styles.foodRating}>
+            <p className={styles.name}>{name}</p>
+            <p className={styles.foodDesc}>{description}</p>
+          </div>
+          <div className={styles.starContainer}>
+            <div className={styles.starDiv}>
+              <StarRateIcon className={styles.star}/> <StarRateIcon className={styles.star}/> <StarRateIcon className={styles.star}/> <StarRateIcon className={styles.star}/> <StarBorderIcon className={styles.star1}/>
+            </div>
+            <p className={styles.foodPrice}>₦ {price}</p>
+          </div>
+          
+        </div>
     </div>
   )
 }
--- client/src/Context/StoreContext.jsx
+++ client/src/Context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { foodList } from "../assets/assts";
 
 
@@ -6,8 +6,28 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
     
+    const [cartItems, setCartItems] = useState({});
+
+    // const addTocart = () =>{
+    //     setCartItems(cartItems + 1);
+    // }
+
+    const addTocart = (foodId) => {
+        if(!cartItems[foodId]){
+            setCartItems((prev)=>({...prev, [foodId]:1}))
+        }
+        else{
+            setCartItems((prev)=>({...prev, [foodId]:prev[foodId] + 1}));
+        }
+    }
+
+    const removeFromCart = (foodId) => {
+        setCartItems((prev)=>({...prev, [foodId]:prev[foodId] - 1}));
+    }
+
+
     const contextValue = {
-        foodList
+        foodList, cartItems, setCartItems, addTocart, removeFromCart
     }
     return (
         <StoreContext.Provider value={contextValue}>

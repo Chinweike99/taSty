49039cb895fb2fb47a4442214f5afe4dee63eee0
Chinweike99 <innocentakwolu1@gmail.com>
2024-12-26 20:01:19 +0100

    Admin nav

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import styles from './Navbar.module.css'
-
+import React from "react";
+import styles from "./Navbar.module.css";
+import { assets } from "../../assets/assets";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className={styles.navbar}>
+      <a  className={styles.logo}>
+        <p>taS😋ty</p>
+        <small>Admin panel..</small>
+      </a>
+      <img src={assets.profileImage_2} alt="profile_image" />
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ admin/src/assets/assets.js
@@ -0,0 +1,6 @@
+import profileImage1 from './profileImage1.jpeg'
+import profileImage_2 from './profileImage_2.jpeg'
+
+export const assets = {
+    profileImage1, profileImage_2
+}

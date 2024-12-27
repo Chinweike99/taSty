ef22b05fb52793bb02677854b27920fe8703595a
Chinweike99 <innocentakwolu1@gmail.com>
2024-12-27 05:44:24 +0100

    admin sidebar

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import styles from './Sidebar.module.css'
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import ListIcon from '@mui/icons-material/List';
+import ReceiptIcon from '@mui/icons-material/Receipt';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className={styles.sidebar}>
+        <div className={styles.options}>
+            <AddBoxIcon />
+            <span>Add food</span>
+        </div>
+        <div className={styles.options}>
+            <ListIcon />
+            <span>Food list</span>
+        </div>
+        <div className={styles.options}>
+            <ReceiptIcon />
+            <span>Orders</span>
+        </div>
+    </div>
   )
 }
 
